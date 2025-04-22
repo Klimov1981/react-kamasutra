6123fb474bbba66bfa804eb1cc5fe8a20754c83d
Al <klimoff.81@mail.ru>
2025-04-22 09:38:32 +0300

    фунциональная компонента Users переделана в классовую

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,50 @@
+import axios from 'axios'
 import './Users.css'
+import userPhoto from "../../assets/images/user.png"
+import React from 'react'
 
-export const Users = (props) => {
+export class Users extends React.Component{
+  // props здесь является частью объекта, они не передаются, поэтому this.props
 
-if (props.users.length === 0) {
-  props.setUsers([
-    { id: 7, photoUrl: 'https://i.pinimg.com/736x/19/d8/d6/19d8d6120cc94f5d7484e537a134084d.jpg', followed: false, fullName: "Sophie Martin", status: "teacher", location: { city: "Paris", citizenship: "France" } },
-    { id: 8, photoUrl: 'https://i.pinimg.com/736x/19/d8/d6/19d8d6120cc94f5d7484e537a134084d.jpg', followed: true, fullName: "Иван Козлов", status: "engineer", location: { city: "Yekaterinburg", citizenship: "RF" } },
-    { id: 9, photoUrl: 'https://i.pinimg.com/736x/19/d8/d6/19d8d6120cc94f5d7484e537a134084d.jpg', followed: false, fullName: "Anna Müller", status: "artist", location: { city: "Berlin", citizenship: "Germany" } },
-    { id: 10, photoUrl: 'https://i.pinimg.com/736x/19/d8/d6/19d8d6120cc94f5d7484e537a134084d.jpg', followed: true, fullName: "Дмитрий Волков", status: "musician", location: { city: "Sochi", citizenship: "RF" } }
-  ])
-}
-
-  return (
-    <>
-
-      {
-        props.users.map(u => <div className='user' key={u.id}>
-          <span className='ava-box'>
-            <div>
-              <img className='user-img' src={u.photoUrl} alt={u.fullName} />
-            </div>
-            <div>
-              {u.followed
-                ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-            </div>
-          </span>
-          <span className='user-info'>
-            <span>
-              <div>{u.fullName}</div>
-              <div>{u.status}</div>
-            </span>
-            <span>
-              <div>{u.location.city}</div>
-              <div>{u.location.citizenship}</div>
-            </span>
-          </span>
-
-        </div>)
+  getUsers = () => {
+      if (this.props.users.length === 0) {
+  
+  
+        axios.get("https://cors-anywhere.herokuapp.com/https://social-network.samuraijs.com/api/1.0/users").then(response => {
+  
+          this.props.setUsers(response.data.items)
+        })
       }
-
-    </>
-  )
+    }
+  render() {
+    return  <>
+          <button onClick={this.getUsers}>Get users</button>
+          {
+            this.props.users.map(u => <div className='user' key={u.id}>
+              <span className='ava-box'>
+                <div>
+                  <img className='user-img' src={u.photos.small != null ? u.photos.small : userPhoto} alt={u.name} />
+                </div>
+                <div>
+                  {u.followed
+                    ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                    : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+                </div>
+              </span>
+              <span className='user-info'>
+                <span>
+                  <div>{u.name}</div>
+                  <div>{u.status}</div>
+                </span>
+                <span>
+                  <div>{"u.location.city"}</div>
+                  <div>{"u.location.citizenship"}</div>
+                </span>
+              </span>
+    
+            </div>)
+          }
+    
+        </>
+  }
 }
